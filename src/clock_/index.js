d524d6868c29387fea2e32dfb66571c496f322f1
Geri <herhel.horvat@teaminternational.com>
2021-12-22 19:08:37 +0200

    module 9.1

---
+++ src/clock_/index.js
@@ -0,0 +1,70 @@
+import './style.css';
+
+const template = ({ title = '' } = {}) => `<div class="clock">
+  <h3>${title}</h3>
+  <p class="date"></p>
+  <p class="time"></p>
+  <button class="btn btn-light stop">Stop</button>
+  <button class="btn btn-success start">Start</button>
+</div>`;
+
+class Clock {
+  #parent;
+  #date;
+  #time;
+  #timerId;
+
+  constructor({ selector, title = '' }) {
+    this.#parent = document.querySelector(selector);
+
+    this.render(title);
+
+    this.#parent
+      .querySelector('.stop')
+      .addEventListener('click', this.stop.bind(this));
+    this.#parent
+      .querySelector('.start')
+      .addEventListener('click', this.start.bind(this));
+  }
+
+  render(title) {
+    this.#parent.insertAdjacentHTML('beforeend', template({ title }));
+    this.#date = this.#parent.querySelector('.date');
+    this.#time = this.#parent.querySelector('.time');
+  }
+
+  update() {
+    const date = new Date();
+
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    const hh = date
+      .getHours()
+      .toString()
+      .padStart(2, 0);
+    const mm = date
+      .getMinutes()
+      .toString()
+      .padStart(2, 0);
+    const ss = date
+      .getSeconds()
+      .toString()
+      .padStart(2, 0);
+
+    this.#date.textContent = `${day}.${month}.${year}`;
+    this.#time.textContent = `${hh}:${mm}:${ss}`;
+  }
+
+  start() {
+    this.#timerId = setInterval(this.update.bind(this), 1000);
+  }
+
+  stop() {
+    clearInterval(this.#timerId);
+    this.#timerId = 0;
+  }
+}
+
+export default Clock;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
-console.log('module 8.1');
+console.log('module 9.1');
 
 import 'bootstrap';
-import toastr from 'toastr';
-
 import 'bootstrap/scss/bootstrap.scss';
 import 'toastr/build/toastr.min.css';
 import 'basiclightbox/dist/basicLightbox.min.css';
 import './style.css';
-
-import './todos';
-
-// ===== localstorage =====
-// localStorage.setItem('test', 'sdfgvsdfgd');
-// localStorage.setItem('test', JSON.stringify({ name: 'Bobby' }));
-// let obj;
-
-// try {
-//   obj = JSON.parse(localStorage.getItem('test'));
-// } catch (e) {
-//   obj = {};
-//   console.log("can't read the local storage");
-// }
-
-// const { name = 'Nemo' } = obj;
-// console.log('My name is', name);
