cb47ed810c1df3683518d498e4af043f8465de13
Geri <herhel.horvat@teaminternational.com>
2021-12-30 21:09:58 +0200

    module 10.2

--- src/search/index.js
+++ src/search/index.js
@@ -1,19 +1,27 @@
+import InfiniteScroll from 'infinite-scroll';
+import qs from 'query-string';
+
 const refs = {
   form: document.querySelector('.form'),
   listGroup: document.querySelector('.list-group'),
   pages: document.querySelector('.pages'),
   prevPage: document.querySelector('a[data-page="prev"]'),
   nextPage: document.querySelector('a[data-page="next"]'),
+  loadMore: document.querySelector('.btn-success'),
 };
 let maxPage = 1;
 let currentPage = 1;
 
 function renderList(hits) {
-  const items = [];
+  const items = hits.map(
+    ({ title, story_title, url, story_url }) =>
+      `<a href="${url ||
+        story_url ||
+        '#'}" class="list-group-item list-group-item-action">${title ||
+        story_title}</a>`,
+  );
 
-  // TODO: generate list here
-
-  refs.listGroup.innerHTML = '';
+  // refs.listGroup.innerHTML = '';
   refs.listGroup.insertAdjacentHTML('beforeend', items.join(''));
 }
 
@@ -21,14 +29,31 @@ function renderList(hits) {
 // https://hn.algolia.com/api/v1/search?query=biocomputer&hitsPerPage=5&page=0
 function fetchNews() {
   const SEARCH_URL = 'https://hn.algolia.com/api/v1/search';
+  const { value } = refs.form.elements.queryText;
+  const params = {
+    query: value,
+    hitsPerPage: 5,
+    page: currentPage - 1,
+  };
+  const strParams = qs.stringify(params);
 
-  console.log('fetch news:', SEARCH_URL);
+  if (!value) return;
+
+  fetch(`${SEARCH_URL}?${strParams}`)
+    .then((resp) => resp.json())
+    .then(({ nbPages, page, hits }) => {
+      maxPage = nbPages;
+      currentPage = page + 1;
+
+      renderList(hits);
+      renderPagination();
+    });
 }
 
 function handleSubmit(e) {
   e.preventDefault();
-  // setUrlParams();
-  // fetchNews();
+  setUrlParams(); // optional
+  fetchNews();
 }
 
 function handleClickPrevPage(e) {
@@ -57,25 +82,55 @@ function handleClickPage(e) {
   fetchNews();
 }
 
+function handleClickLoadMore() {
+  currentPage++;
+  fetchNews();
+}
+
 function addEventListeners() {
   refs.form.addEventListener('submit', handleSubmit);
   refs.prevPage.addEventListener('click', handleClickPrevPage);
   refs.nextPage.addEventListener('click', handleClickNextPage);
   refs.pages.addEventListener('click', handleClickPage);
+  refs.loadMore.addEventListener('click', handleClickLoadMore);
 }
 
+// optional
 function getUrlParams() {
-  // TODO: code here
+  const { query } = qs.parse(window.location.search);
+
+  refs.form.elements.queryText.value = query;
 }
 
+// optional
 function setUrlParams() {
-  // TODO: code here
+  const { value } = refs.form.elements.queryText;
+
+  window.history.pushState('', '', `${window.location.origin}\?query=${value}`);
 }
 
 function renderPagination() {
   const items = [];
 
-  // TODO: generate items here
+  for (let i = 1; i <= maxPage; i += 1) {
+    items.push(
+      `<li class="page-item ${
+        i === currentPage ? 'active' : ''
+      }"><a class="page-link" href="#" data-page=${i}>${i}</a></li>`,
+    );
+  }
+
+  if (currentPage === 1) {
+    refs.prevPage.closest('li').classList.add('disabled');
+  } else {
+    refs.prevPage.closest('li').classList.remove('disabled');
+  }
+
+  if (currentPage === maxPage) {
+    refs.nextPage.closest('li').classList.add('disabled');
+  } else {
+    refs.nextPage.closest('li').classList.remove('disabled');
+  }
 
   refs.pages.innerHTML = '';
   refs.pages.insertAdjacentHTML('beforeend', items.join(''));
@@ -83,8 +138,8 @@ function renderPagination() {
 
 function start() {
   addEventListeners();
-  // getUrlParams(); // optional
-  // fetchNews();
+  getUrlParams(); // optional
+  fetchNews();
 }
 
 start();
